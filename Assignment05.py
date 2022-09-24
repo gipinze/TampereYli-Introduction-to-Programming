9ae7f3f63a62eb711d2e01e53b2e39b0523a0c1a
Gianmarco Ipinze <48326351+gipinze@users.noreply.github.com>
2022-09-25 00:47:58 +0300

    Create Assignment05.py

---
+++ Assignment05.py
@@ -0,0 +1,36 @@
+"""
+The first program of Programming 01 2022
+Name: Gianmarco Ipinze Tutuianu <gianmarco.ipinzetutuianu@tuni.fi>
+StudentId: 151456655
+
+"""
+
+# A program that determines user's age group (if-statement).
+def main():
+    # Read the age as a string and convert it to an integer.
+    string = input("How do you feel? (1-10) ")
+    feel = int(string)
+    if feel > 0 and feel <= 10:
+        
+    # Determine the suitable smile group.
+    
+        if feel == 1:
+            print("A suitable smiley would be :'(")
+        
+        elif feel < 4:
+            print("A suitable smiley would be :-(")
+            
+        elif feel < 8:
+            print("A suitable smiley would be :-|")
+
+        elif feel > 7 and feel < 10:
+            print("A suitable smiley would be :-)")
+            
+        else:
+            print("A suitable smiley would be :-D")
+    
+    else:
+        print("Bad input!")
+
+if __name__ == "__main__":
+    main()
