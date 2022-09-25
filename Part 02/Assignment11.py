91f9dc8c4cdb21d15f77b126e62e61a2fc2aab2b
Gianmarco Ipinze <48326351+gipinze@users.noreply.github.com>
2022-09-25 11:00:11 +0300

    Update and rename assignmente11.py to Assignment11.py

--- Part 02/Assignment11.py
+++ Part 02/Assignment11.py
@@ -1,10 +1,3 @@
-"""
-The first program of Programming 01 2022
-Name: Gianmarco Ipinze Tutuianu <gianmarco.ipinzetutuianu@tuni.fi>
-StudentId: 151456655
-
-"""
-
 def main():
 
     n = int(input("Choose a number: "))
